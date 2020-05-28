b658d93d82f65288c6bf2fe00d9b6ce41cd0f3d3
pmontens <pieterjan@montens.net>
2020-05-28 14:46:38 +0200

    :wrench: added bootstrap, sass, react-icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,23 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "../styles/style.scss"
+import Button from 'react-bootstrap/Button';
+
+import { FaBeer } from 'react-icons/fa';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Anon Api Test" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site. Want a <FaBeer /> ?</p>
     <p>Now go build something great.</p>
+    <p className="poulet">Or try something<span> new !</span></p>
+    <Button>Coucou</Button>
+    <div className="row">
+        <div className="col-6">Bouh</div>
+        <div className="col-4">Bouh</div>
+    </div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
