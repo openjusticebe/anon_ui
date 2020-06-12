5fe8e4b36dc71f26e4dc5f715063a710ac4df009
pmontens <pieterjan@montens.net>
2020-06-12 15:43:22 +0200

    :wrench: fixing config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
       options: {
         typeName: "query",
         fieldName: "api",
-        url: `http://localhost:5000/gql/`,
-        //url: `${process.env.GATSBY_API_URL}/gql/`,
+        //url: `http://localhost:5000/gql/`,
+        url: `${process.env.GATSBY_API_URL}/gql/`,
         refetchInterval: 60,
       },
     },
